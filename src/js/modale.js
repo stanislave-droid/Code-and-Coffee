027ed57c6516e1a1bb9c1c5a587674e08ba13cda
Evgen Guijeen <guijeenua@gmail.com>
2026-02-08 23:19:55 +0200

    Cheking email

--- src/js/modale.js
+++ src/js/modale.js
@@ -1,20 +1,28 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
     openModalBtn: document.querySelector('.subscribe-form-button'),
-    // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-modal-close]'),
     closeModalButton: document.querySelector('.modal-btn'),
-    // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', event => {
+    const input = document.querySelector('.subscribe-form-input');
+    event.preventDefault();
+
+    if (!input.checkValidity()) {
+      alert('Помилка: Поле Email заповнене не вірно!');
+    } else {
+      toggleModal();
+      console.log(`Email користувача - ${input.value}`);
+    }
+    input.value = '';
+  });
+
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.closeModalButton.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('subscription-open');
   }
 })();
